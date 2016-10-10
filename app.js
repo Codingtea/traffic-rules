7d843c21e12b4d06ee08eb96621812a30909938d
Angelo Pearce <angelopearce85@gmail.com>
2016-10-10 10:09:06 +0200

    Traffedit

--- app.js
+++ app.js
@@ -4,31 +4,44 @@ var body = document.querySelector("body");
 
 var taxiLocationCounter = 1;
 
+
+
+
 body.onkeydown = function(e){
 
-    //right arrow : 39
-    //left arrow  : 37
-    //up arrow    : 38
+  if (e.keyCode === 39 ){
+
+var previousLocationCounter = taxiLocationCounter;
+    taxiLocationCounter++
+    moveTaxi(previousLocationCounter, taxiLocationCounter)
+    displayMessage(e.keyCode);
+  }
+  else if (e.keyCode === 37 ){
+
+  var previousLocationCounter = taxiLocationCounter;
+      taxiLocationCounter--
+      moveTaxi(previousLocationCounter, taxiLocationCounter)
+      displayMessage(e.keyCode);
+  }
+      //up arrow    : 38
+
+
+    // down  : 37
+
+
     //down arrow  : 40
 
     displayMessage(e.keyCode);
 
-    /*
 
-    to move the taxi forward...
-    when the right arrow is pressed
-    work with `taxiLocationCounter`
-
-    store the value of `taxiLocationCounter` in a variable called `previousLocationCounter`
-    increment the `taxiLocationCounter`
-
-    pass both to the `moveTaxi( previousLocationCounter,
-        taxiLocationCounter)` function
-
-    */
 
     // to make the second TrafficLight `Orange`
-    var tl = new TrafficLight(2);
-    tl.orange();
+    // var tl = new TrafficLight(2);
+    // tl.orange();
+
+
+    var trafficLight = new TrafficLight(taxiLocationCounter);
+    trafficLight.red();
+     trafficLight.state();
 
 };
